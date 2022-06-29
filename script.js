f77f255e1950a7419bb6bfa38c2a1dcc8df465cc
joab <j.torreys@outlook.com>
2022-06-28 17:26:07 -0800

    ok weather api

---
+++ script.js
@@ -0,0 +1,23 @@
+function run() {
+  let api = "e78e3b483e33573d96521a5aa1ccdfd1";
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    let x = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}`;
+
+    // going for fetch now
+    fetch(x).then((response) => {
+      console.log(response);
+    });
+  });
+}
+
+run();
+
+// function name(params) {
+//   let str;
+//   str = "joab";
+//   console.log(`hello worl${str}is a${str} boss`);
+// }
+// name();
