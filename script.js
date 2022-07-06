826ec65ce95751b428361f8eee8bae67a2ac5710
joab <j.torreys@outlook.com>
2022-07-06 15:12:09 -0800

    ok

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function run() {
   let api = "e78e3b483e33573d96521a5aa1ccdfd1";
+  const winput = document.getElementById("one");
+  winput.innerHTML = "fffhufufh";
+  const tinput = document.getElementById("two");
+  winput.innerHTML = "fffhufufh";
+  const binput = document.getElementById("three");
+  // next
 
+  tinput.innerHTML = "38%";
+  binput.innerHTML = "heavy";
+  // next
   navigator.geolocation.getCurrentPosition((position) => {
     lat = position.coords.latitude;
     long = position.coords.longitude;
@@ -10,7 +19,8 @@ function run() {
     fetch(x)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        console.log(data.weather[0]);
+        winput.innerHTML = data.name;
       });
   });
 }
